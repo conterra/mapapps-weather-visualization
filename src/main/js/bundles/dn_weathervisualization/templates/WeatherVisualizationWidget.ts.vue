343b47df334dc36e03f386800a3aa90b25968b41
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-03-28 11:05:43 +0100

    Remove testing variable

--- src/main/js/bundles/dn_weathervisualization/templates/WeatherVisualizationWidget.ts.vue
+++ src/main/js/bundles/dn_weathervisualization/templates/WeatherVisualizationWidget.ts.vue
@@ -231,7 +231,6 @@
             handleWeatherChange(weatherType: string): void {
                 this.activeWeather = weatherType;
                 this.$emit('weather-change', this.activeWeather);
-                this.i18n;
             }
         }
     });
